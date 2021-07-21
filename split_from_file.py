0be9d7fa33acbf0e2ee925ce924c5262729993d6
yobranodev <yobranoDev@gmail.com>
2021-07-21 17:16:28 +0300

    Initial commit

---
+++ split_from_file.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import os, argparse
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+
+def save_pdf(writer, name, mkdirs):
+    """ Saves out the pdf splits """
+
+    file_name = f'{name}.pdf'
+    if mkdirs:
+        try:
+            os.mkdir(name)
+            file_name = f'{name}/{name}.pdf'
+
+        except Exception as e:
+            file_name = f'{name}/{name}.pdf'
+
+    with open(file_name, 'wb') as f:
+        writer.write(f)
+
+
+def split_by_row(row, pdf= None, mkdir= True):
+    """ Split the pdf based on row  """
+    name, start, end = row['name'], row['start'], row['end']
+    name= name.strip()
+    writer = PdfFileWriter()
+
+    [writer.addPage(pdf.getPage(pg)) for pg in range(start-1, end)]
+    save_pdf(writer, name, mkdir)
+
+    print(f'{name:20} | {start:5} | {end:5}')
+
+def splitter(mkdir= True, pdf= None, df= None):
+    """ Use loaded files to split. """
+
+    df.apply( split_by_row,
+              axis= 1, 
+              pdf= pdf,
+              mkdir= mkdir)
+
+def main(pdf_file, csv_file, no_dirs = False):
+    df = pd.read_csv(csv_file)
+    df = df[['name', 'start', 'end']]  
+    print('Splits DataFrame Loaded...')
+
+    pdf  = PdfFileReader(open(pdf_file, 'rb'))
+    print('Pdf Loaded...\n')
+
+    splitter(df= df, pdf= pdf, mkdir= not no_dirs)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("PdfInput", help = "Pass in a pdf file to be splited.")
+    parser.add_argument("CsvInput", help = "Pass in a Csv file with split rows.")
+    parser.add_argument('--no-dirs', action= "store_true", help= "Don't create directories for each split")
+
+    args = parser.parse_args() 
+    
+    main( args.PdfInput, args.CsvInput, no_dirs= args.no_dirs)
+
+
